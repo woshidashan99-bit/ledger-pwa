ae8b394bbf6d22cf0506655c1e15769ead101093
woshidashan99-bit <woshidashan99@gmail.com>
2026-03-02 15:53:38 +0800

    Add files via upload
    第一次初版生成

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "ledger-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
